befef265c568292e492fa78fb3ca1a4883313910
sangram123sa <aparnasamanta90028@gmail.com>
2025-04-22 23:26:33 +0530

    first push

---
+++ routes/public.js
@@ -0,0 +1,13 @@
+const {Router} = require('express');
+
+const router = Router()
+
+router.get("/signin", (req, res)=>{
+    return res.render('signin');
+})
+
+router.get("/signup", (req, res)=>{
+    return res.render("signup");
+})
+
+module.exports = router
